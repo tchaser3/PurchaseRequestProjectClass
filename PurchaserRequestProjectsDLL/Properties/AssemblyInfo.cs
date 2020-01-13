9efc031e71de350bc88f300d0780c3ed1bbd5df4
Terry Holmes <tchaser3@outlook.com>
2020-01-13 14:35:19 -0500

    Add project files.

---
+++ PurchaserRequestProjectsDLL/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PurchaserRequestProjectsDLL")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PurchaserRequestProjectsDLL")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e33361da-c6b9-49b9-b73d-432e35313cd3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PurchaserRequestProjectsDLL/PurchaseRequestProjectsClass.cs
@@ -0,0 +1,107 @@
+﻿/* Title:           Purchase Request Projects Class
+ * Date:            1-13-20
+ * Author:          Terry HOlmes
+ * 
+ * Description:     This class is used for the purchase request projects */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NewEventLogDLL;
+
+namespace PurchaserRequestProjectsDLL
+{
+    public class PurchaseRequestProjectsClass
+    {
+        EventLogClass TheEventLogClass = new EventLogClass();
+
+        PurchaseRequestProjectsDataSet aPurchaseRequestProjectsDataSet;
+        PurchaseRequestProjectsDataSetTableAdapters.purchaserequestprojectsTableAdapter aPurchaseRequestProjectsTableAdapter;
+
+        InsertPurchaseRequestProjectEntryTableAdapters.QueriesTableAdapter aInsertPurchaseRequestProjectTableAdapter;
+
+        FindPurchaseRequestProjectByJobNumberDataSet aFindPurchaseRequestProjectByJobNumberDataSet;
+        FindPurchaseRequestProjectByJobNumberDataSetTableAdapters.FindPurchaseRequestProjectByJobNumberTableAdapter aFindPurchaseRequestProjectByJobNumberTableadapter;
+
+        FindPurchaseRequestProjectByProjectIDDataSet aFindPurchaseRequestProjectByProjectIDDataSet;
+        FindPurchaseRequestProjectByProjectIDDataSetTableAdapters.FindPurchaseRequestProjectByProjectIDTableAdapter aFindPurchaseRequestProjectByProjectIDTableAdapter;
+
+        public FindPurchaseRequestProjectByProjectIDDataSet FindPurchaseRequestByProjectID(int intProjectID)
+        {
+            try
+            {
+                aFindPurchaseRequestProjectByProjectIDDataSet = new FindPurchaseRequestProjectByProjectIDDataSet();
+                aFindPurchaseRequestProjectByProjectIDTableAdapter = new FindPurchaseRequestProjectByProjectIDDataSetTableAdapters.FindPurchaseRequestProjectByProjectIDTableAdapter();
+                aFindPurchaseRequestProjectByProjectIDTableAdapter.Fill(aFindPurchaseRequestProjectByProjectIDDataSet.FindPurchaseRequestProjectByProjectID, intProjectID);
+            }
+            catch (Exception Ex)
+            {
+                TheEventLogClass.InsertEventLogEntry(DateTime.Now, "Purchase Request Project Class // Find Purchase Request By Project ID " + Ex.Message);
+            }
+
+            return aFindPurchaseRequestProjectByProjectIDDataSet;
+        }
+        public FindPurchaseRequestProjectByJobNumberDataSet FindPurchaseRequestProjectByJobNumber(string strJobNumber)
+        {
+            try
+            {
+                aFindPurchaseRequestProjectByJobNumberDataSet = new FindPurchaseRequestProjectByJobNumberDataSet();
+                aFindPurchaseRequestProjectByJobNumberTableadapter = new FindPurchaseRequestProjectByJobNumberDataSetTableAdapters.FindPurchaseRequestProjectByJobNumberTableAdapter();
+                aFindPurchaseRequestProjectByJobNumberTableadapter.Fill(aFindPurchaseRequestProjectByJobNumberDataSet.FindPurchaseRequestProjectByJobNumber, strJobNumber);
+            }
+            catch (Exception Ex)
+            {
+                TheEventLogClass.InsertEventLogEntry(DateTime.Now, "Purchase Request Project Class // Find Purchase Request Project By Job Number " + Ex.Message);
+            }
+
+            return aFindPurchaseRequestProjectByJobNumberDataSet;
+        }
+        public bool InsertPurchaseRequestProject(int intPONumber, string strJobNumber, int intProjectID)
+        {
+            bool blnFatalError = false;
+
+            try
+            {
+                aInsertPurchaseRequestProjectTableAdapter = new InsertPurchaseRequestProjectEntryTableAdapters.QueriesTableAdapter();
+                aInsertPurchaseRequestProjectTableAdapter.InsertPurchaseRequestProject(intPONumber, strJobNumber, intProjectID);
+            }
+            catch (Exception Ex)
+            {
+                TheEventLogClass.InsertEventLogEntry(DateTime.Now, "Purchase Request Projects Class // Insert Purchase Request Project " + Ex.Message);
+
+                blnFatalError = true;
+            }
+
+            return blnFatalError;
+        }
+        public PurchaseRequestProjectsDataSet GetPurchaseRequestProjectsInfo()
+        {
+            try
+            {
+                aPurchaseRequestProjectsDataSet = new PurchaseRequestProjectsDataSet();
+                aPurchaseRequestProjectsTableAdapter = new PurchaseRequestProjectsDataSetTableAdapters.purchaserequestprojectsTableAdapter();
+                aPurchaseRequestProjectsTableAdapter.Fill(aPurchaseRequestProjectsDataSet.purchaserequestprojects);
+            }
+            catch (Exception Ex)
+            {
+                TheEventLogClass.InsertEventLogEntry(DateTime.Now, "Purchase Reqeust Projects Class // Get Purchase Request Projects Info " + Ex.Message);
+            }
+
+            return aPurchaseRequestProjectsDataSet;
+        }
+        public void UpdatePurchaseRequestProjectsDB(PurchaseRequestProjectsDataSet aPurchaseRequestProjectsDataSet)
+        {
+            try
+            {
+                aPurchaseRequestProjectsTableAdapter = new PurchaseRequestProjectsDataSetTableAdapters.purchaserequestprojectsTableAdapter();
+                aPurchaseRequestProjectsTableAdapter.Update(aPurchaseRequestProjectsDataSet.purchaserequestprojects);
+            }
+            catch (Exception Ex)
+            {
+                TheEventLogClass.InsertEventLogEntry(DateTime.Now, "Purchase Reqeust Projects Class // Update Purchase Request Projects DB " + Ex.Message);
+            }
+        }
+    }
+}
